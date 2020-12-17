706e9f9107f3ece55cd3b6cc0c1b097e3aeed87a
Rahul Rao <RAOR378647@exactsoftware.com>
2020-12-17 14:07:11 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-observable-error';
+  srcArray = from([1, 2, 'A', 4]);
+
+  obs = this.srcArray
+    .pipe(
+      map(val => {
+        let result = val as number * 2;
+        if (Number.isNaN(result)) {
+          console.log('Error Occurred in Stream')
+          throw new Error("Result is NaN")
+        }
+        return result
+      }),
+    );
+
+  ngOnInit() {
+
+    this.obs.subscribe(
+      el => {
+        console.log('Value Received ' + el)
+      },
+      err => {
+        console.log("Error Returned to Subscriber " + err)
+      },
+      () => console.log("Processing Complete.")
+    )
+  }
 }
